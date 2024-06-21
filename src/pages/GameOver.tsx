d1d55db37a2355acc8aed4bdfc0b16ed3fd74d17
Loïc Cape <loiccape.pro@outlook.fr>
2024-06-21 20:40:28 +0200

    ajout bouton resplay

--- src/pages/GameOver.tsx
+++ src/pages/GameOver.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 export default function GameOver() {
   const { result } = useParams<{ result: string }>();
@@ -9,6 +9,9 @@ export default function GameOver() {
       <div className="text-center p-8 bg-gray-800 rounded-lg shadow-lg">
         <h1 className="text-4xl font-bold text-white mb-4">Game Over</h1>
         <p className="text-2xl text-gray-300">Vous avez : <span className="font-semibold text-white">{result}</span></p>
+        <Link to='/'>
+        <button className='mt-4 px-6 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow hover:bg-blue-600'>Relancer une partie</button>
+        </Link>
       </div>
     </div>
   );
